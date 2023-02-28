b9dbc5385748e9ab4c79e20545e7230aae7adad1
Frankie Lam <frankie191193@gmail.com>
2023-02-28 10:43:34 +0000

    set layout theme to system preference with the option of client-side toggling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,33 @@
+"use client";
 import "./globals.css";
+import { useState, useEffect } from "react";
 
-export const metadata = {
-  title: "Firefly Energy",
-  description:
-    "Access your account to see your bills, consumption, meter readings and account details.",
-};
+function getUserThemePreference() {
+  const isDarkModePreferred = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  return isDarkModePreferred ? "dark" : "light";
+}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
+
+  useEffect(() => {
+    const themePreference = getUserThemePreference();
+
+    setTheme(themePreference);
+  }, []);
+
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={theme}>
+      <body className="bg-white dark:bg-black text-black dark:text-white">
+        {children}
+      </body>
     </html>
   );
 }
