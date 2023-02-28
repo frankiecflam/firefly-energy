dc4aa661f531dfe285b063b8b8ad544c06a07cf6
Frankie Lam <frankie191193@gmail.com>
2023-02-28 10:26:20 +0000

    override default colors of black and white from tailwindCSS with custom values

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -14,6 +15,22 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        black: {
+          DEFAULT: "#1D1B1B",
+          100: "#2E2A2A",
+          200: "#3F3A3A",
+          300: "#504A4A",
+          400: "#615959",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
+          100: "#EDEDED",
+          200: "#DBDBDB",
+          300: "#C8C8C8",
+          400: "#B6B6B6",
+        },
       },
     },
   },
