d9e114fa18f1a479cb3ea4d9301f3aff3b5cce89
Frankie Lam <frankie191193@gmail.com>
2023-03-02 19:20:35 +0000

    finishd dashboardMeterReadings with static data

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+import dynamic from "next/dynamic";
 import DashboardBalance from "@/components/dashboard/dashboardBalanceCard";
 import DashboardConsumption from "@/components/dashboard/dashboardConsumptionCard";
-import DashboardMeterReadings from "@/components/dashboard/dashboardMeterReadings";
 import DashboardPayments from "@/components/dashboard/dashboardPayments";
 import DashboardAccount from "@/components/dashboard/dashboardAccount";
+import DashboardCard from "@/components/dashboard/dashboardCard";
+
+const DashboardMeterReadings = dynamic(
+  () => import("@/components/dashboard/dashboardMeterReadings"),
+  {
+    ssr: false,
+    loading: () => (
+      <DashboardCard title="meter readings" className="row-span-2">
+        <div className="mx-auto flex h-[350px] w-[300px] items-center justify-center text-2xl text-black-100 dark:text-white-100">
+          Loading...
+        </div>
+      </DashboardCard>
+    ),
+  }
+);
 
 export const metadata = {
   title: "Firefly Energy - Dashboard",
@@ -13,7 +28,7 @@ export const metadata = {
 export default function Dashboard() {
   return (
     <section className="min-h-[700px]">
-      <header className="flex flex-col items-start">
+      <header className="flex flex-col">
         <h1 className="py-2 text-xl font-light capitalize tracking-wider text-black-200 dark:text-white-200">
           dashboard
         </h1>
@@ -21,7 +36,7 @@ export default function Dashboard() {
           Hello, Frankie Lam 👋🏻
         </p>
       </header>
-      <div className="mt-10 grid grid-rows-3 gap-y-12 lg:grid-cols-2 lg:gap-6">
+      <div className="mt-10 grid gap-y-12 lg:grid-cols-2 lg:grid-rows-3  lg:items-start lg:gap-6">
         <DashboardBalance />
         <DashboardConsumption />
         <DashboardMeterReadings />
